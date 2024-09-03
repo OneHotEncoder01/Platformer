648b3da3df562873ca59cbeaf01d9a4e195dadc1
sinanotuk711 <sinanotuk711@penguin>
2024-09-03 14:36:47 +0200

    Initial file structure

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+// Author: Your Name
+/*
+ * main.cpp
+ * Copyright (C) 2024 sinanotuk711 <sinanotuk711@penguin>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "main.h"
+
+
+// Copyright: 2024
+// Description: 
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+
